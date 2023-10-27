ff3e9f53866c0f7624cea66d279e386a06936838
HAMBISA ADAMU <117825147+GEDIH@users.noreply.github.com>
2023-10-27 08:25:50 +0300

    Add files via upload

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,41 +1,25 @@
 #!/usr/bin/python3
-'''Module to find Pascal's Triangle integers'''
+"""Pascal Triangle Interview Challenge"""
 
 
 def pascal_triangle(n):
-    '''
-    Function to find Pascal's Triangle integers
-
-        Parameters:
-            n (int): The number of row's of Pascal's triangle
-
-        Returns:
-            pascal_triangle (list): Binary string of the sum of a and b
-    '''
-    pascal_triangle = list()
-
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
     if n <= 0:
-        return pascal_triangle
+        return []
 
-    # Add first 1.
-    if n > 0:
-        pascal_triangle.append([1])
+    pascal_t = []
 
-    # Add second line.
-    if n > 1:
-        pascal_triangle.append([1, 1])
+    for i in range(n):
+        pascal_t.append([])
+        pascal_t[i].append(1)
 
-    for x in range(3, n+1):
-        pascal_triangle.append([0] * x)
+        for j in range(1, i):
+            x = pascal_t[i-1][j-1]
+            y = pascal_t[i-1][j]
+            pascal_t[i].append(x+y)
 
-        # Set first and last 1
-        pascal_triangle[x-1][0] = 1
-        pascal_triangle[x-1][x-1] = 1
+        if(n != 0 and i != 0):
+            pascal_t[i].append(1)
 
-        # Calculate middle numbers
-        for y in range(1, x-1):
-            pascal_triangle[x-1][y] = \
-                pascal_triangle[x-2][y-1] + pascal_triangle[x-2][y]
-
-    return pascal_triangle
-    
+    return pascal_t
